e5204b0cdfd95347ba85297e02d33f752584d7b6
Arbeits76 <theodoreduchemin@gmail.com>
2024-03-15 11:14:32 +0100

    retirer les warning

--- App.tsx
+++ App.tsx
@@ -1,3 +1,6 @@
+import { LogBox } from 'react-native';
+LogBox.ignoreLogs(['Warning: ...']); // Ignore log notification by message
+LogBox.ignoreAllLogs();//Ignore all log notifications
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 import {
   SafeAreaProvider,
